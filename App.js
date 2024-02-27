cf362bbaa593867a4b6922c51868aa196ef07333
balakaruppannan <144997869+karuppannanbalamuruga@users.noreply.github.com>
2024-02-27 18:54:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import AppBar from './Components/AppBar';
+import Asked from './Components/Asked';
+import Banner from './Components/Banner';
+import Browers from './Components/Browers';
+import Footer from './Components/Footer';
+import Save from './Components/Save';
+import SmartCar from './Components/SmartCar';
+import Trusted from './Components/Trusted';
+import Works from './Components/Works';
+
+function App() {
+  return (
+    <>
+    <AppBar/>
+    <Banner/>
+    <SmartCar/>
+    <Works/>
+    <Save/>
+    <Trusted/>
+    <Asked/>
+    <Footer/>
+    </>
+    
+  );
+}
+
+export default App;
